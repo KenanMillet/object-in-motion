322d150ce66290dfaffea167535919faccc07320
Kenan Millet <kvmillet@gmail.com>
2025-01-23 22:27:28 -0500

    Starting Agent now aims correctly

--- AI_StartingAgent.gd
+++ AI_StartingAgent.gd
@@ -3,6 +3,7 @@ extends Node
 @export var teleportCooldown: float = 6
 @export var teleportRecoveryTime: Vector2
 @export var preferredDistance: Vector2
+@export var aimAssist: AimAssist
 @export var agent: Agent
 
 var recoveringFromTeleport: bool = false
@@ -14,7 +15,6 @@ func teleport() -> void:
 		agent.linear_velocity = Vector2.ZERO
 		agent.angular_velocity = 0
 		agent.global_position = tele_loc
-		agent.rotation = randf_range(0, 2*PI)
 		var recovery_time = randf_range(teleportRecoveryTime.x, teleportRecoveryTime.y)
 		await get_tree().create_timer(recovery_time).timeout
 		recoveringFromTeleport = false
@@ -27,6 +27,13 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if agent.controllingPlayer == null && agent.target != null && agent.gun != null && !recoveringFromTeleport:
-		agent.gun.fire()
+func _physics_process(_delta: float) -> void:
+	if agent.controllingPlayer == null && agent.target != null && agent.gun != null:
+		var gun_dist_sq = agent.global_position.distance_squared_to(agent.gun.global_position)
+		var gun_direction = aimAssist.shotLead(agent.global_position, agent.target.global_position, agent.linear_velocity, agent.target.linear_velocity, agent.gun.bulletSpeed)
+		if gun_direction != Vector2.INF:
+			agent.aimPosition = agent.global_position + (gun_direction * gun_dist_sq)
+		#print(agent.name, " | Target pos (relative): ", agent.target.global_position-agent.global_position, "  Target vel (relative): ", agent.target.linear_velocity-agent.linear_velocity, "  Bullet speed: ", agent.gun.bulletSpeed, "  Gun Direction: ", gun_direction)
+		agent.look_at(agent.target.global_position)
+		if !recoveringFromTeleport:
+			agent.gun.fire()
